9701575fdfe18f0d7e65e6bf2150e4b154984e58
Cooper <45991010+glowbase@users.noreply.github.com>
2024-09-17 09:27:29 +1000

    Checks if users have passwords set.

--- baseline.sh
+++ baseline.sh
@@ -14,11 +14,10 @@ log_header() {
   local heading=$1
   local sub_heading=$2
 
-  # No subheading
   if [ -z "$sub_heading" ]; then
-    echo  -e "${BLUE}${DIV}|${BOLD}${GREEN} ${heading} ${RESET}${BLUE}|${DIV}${RESET}"
+    echo -e "${BLUE}${DIV}|${BOLD}${GREEN} ${heading} ${RESET}${BLUE}|${DIV}${RESET}"
   else
-    echo  -e "${BLUE}${DIV}|${BOLD}${GREEN} ${heading} - ${sub_heading} ${RESET}${BLUE}|${DIV}${RESET}"
+    echo -e "${BLUE}${DIV}|${BOLD}${GREEN} ${heading} - ${sub_heading} ${RESET}${BLUE}|${DIV}${RESET}"
   fi
 }
 
@@ -36,11 +35,9 @@ get_file_property() {
   echo $(cat $file | grep $key | cut -d '"' -f 2)
 }
 
-# Set timezone to UTC
-export TZ="UTC"
-
 # Use the provided mount point
 MOUNT_POINT="$1"
+TIME_ZONE="$2"
 
 # Check if the mount point exists
 if [ ! -d "$MOUNT_POINT" ]; then
@@ -48,7 +45,20 @@ if [ ! -d "$MOUNT_POINT" ]; then
   exit 1
 fi
 
+# Set timezone
+timezone="$(realpath $MOUNT_POINT/etc/localtime | sed 's/.*zoneinfo\///')"
+
+# Check if the mount point exists
+if [ ! "$TIME_ZONE" ]; then
+	export TZ="$timezone"
+else 
+	export TZ="$TIME_ZONE"
+fi
+
+
+
 echo $(log_header "DEVICE SETTINGS")
+echo 
 
 # -----------------------------------------------------------
 # OS VERSION
@@ -65,54 +75,70 @@ else
   os_version="Not Found"
 fi
 
-echo $(log_value "OS Version" "${os_version}")
+echo $(log_value "OS Version" "$os_version")
 
 # -----------------------------------------------------------
 # KERNEL VERSION
 # -----------------------------------------------------------
 kernel_ver=$(ls $MOUNT_POINT/lib/modules | cut -d "." -f 1-4)
-echo $(log_value "Kernel Version" "${kernel_ver}")
+echo $(log_value "Kernel Version" "$kernel_ver")
 
 # -----------------------------------------------------------
 # PROCESSOR ARCHITECTURE
 # -----------------------------------------------------------
 proc_arch=$(ls $MOUNT_POINT/lib/modules | cut -d "." -f 5 )
-echo $(log_value "Processor Architecture" "${proc_arch}")
+echo $(log_value "Processor Architecture" "$proc_arch")
 
 # -----------------------------------------------------------
 # TIME ZONE
 # -----------------------------------------------------------
-# https://forums.ni.com/t5/NI-Linux-Real-Time-Discussions/Timezones/td-p/3693784
-
-timezone="$(realpath $MOUNT_POINT/etc/localtime | sed 's/.*zoneinfo\///')"
-echo $(log_value "Time Zone" "${timezone}")
+echo $(log_value "Time Zone" "$timezone")
 
 # -----------------------------------------------------------
 # LAST SHUTDOWN
 # -----------------------------------------------------------
-last_shutdown=$( last -x -f $MOUNT_POINT/var/log/wtmp | grep shutdown | head -n 1 | cut -d " " -f 7,9-10)
-echo $(log_value "Last Shutdown" "${last_shutdown}")
+last_shutdown=$(last -x -f $MOUNT_POINT/var/log/wtmp | grep shutdown | head -n 1 | cut -d " " -f 7,9-10)
+echo $(log_value "Last Shutdown" "$last_shutdown")
+
+# -----------------------------------------------------------
+# HOSTNAME
+# -----------------------------------------------------------
+hostname=$(cat $MOUNT_POINT/etc/hostname)
+echo $(log_value "Hostname" "$hostname")
+
+echo
 
 # -----------------------------------------------------------
 # USERS
 # -----------------------------------------------------------
 echo $(log_header "USERS")
+echo
 
-users=$(grep "/home" $MOUNT_POINT/etc/passwd | cut -d ":" -f 1,3)
+users=$(egrep "bash|zsh" $MOUNT_POINT/etc/passwd | cut -d ":" -f 1,3)
 
 for userid in $users; do
   id=$(echo "${userid}" | cut -d ":" -f 2)
   user=$(echo "${userid}" | cut -d ":" -f 1)
 
+  password="$(egrep "^$user:[^!][^:]*:.*$" $MOUNT_POINT/etc/shadow)"
   groups=$(cat $MOUNT_POINT/etc/group | grep $user | cut -d ":" -f 1 | sed ':a;N;$!ba;s/\n/, /g')
 
-  echo -e "${CYAN}${BOLD}$user${RESET} ($id): $groups"
+  if [ $password ]; then
+    echo -e "${RED}*${RESET}${CYAN}${BOLD}$user${RESET} ($id): $groups"
+  else
+    echo -e "${CYAN}${BOLD}$user${RESET} ($id): $groups"
+  fi
 done
+
+echo
+echo -e "${RED}*${RESET} Indicates User with Password"
+echo
 
 # -----------------------------------------------------------
 # SUDOERS
 # -----------------------------------------------------------
 echo $(log_header "SUDOERS")
+echo
 
 if [ -f "$MOUNT_POINT/etc/sudoers" ]; then
   sudoers="$(grep -v "#" $MOUNT_POINT/etc/sudoers | egrep -v "^$|Defaults")"
@@ -122,10 +148,13 @@ else
   echo $(log_value "'/etc/sudoers' file not found..." "")
 fi
 
+echo
+
 # -----------------------------------------------------------
 # INSTALLED SOFTWARE
 # -----------------------------------------------------------
 echo $(log_header "INSTALLED SOFTWARE")
+echo
 
 if [ -f "$MOUNT_POINT/var/log/dpkg.log" ]; then
  echo
@@ -138,10 +167,13 @@ else
   echo $(log_value "Could't find Installed Software..." "")
 fi
 
+echo
+
 # -----------------------------------------------------------
 # CRON JOBS
 # -----------------------------------------------------------
 echo $(log_header "CRON JOBS")
+echo
 
 suspicious_patterns=(
     "wget"
@@ -204,6 +236,8 @@ scan_cron_jobs() {
 				echo "$line"
 			fi
         done < "$path"
+
+		echo
     elif [ -d "$path" ]; then
         for file in "$path"/*; do
             if [ -f "$file" ]; then
@@ -214,6 +248,8 @@ scan_cron_jobs() {
                         echo "$line"
                     fi
                 done < "$file"
+
+				echo
             fi
         done
     fi
@@ -241,11 +277,51 @@ done
 if [ "$found_suspicious" = false ]; then
     echo "No Suspicious Crons Found..."
 fi
+
 # -----------------------------------------------------------
 # NETWORK
 # -----------------------------------------------------------
 echo $(log_header "NETWORK")
+echo
 
+echo $(log_value "Hosts" "")
+hosts=$(cat $MOUNT_POINT/etc/hosts)
+echo "$hosts"
+echo
 
+echo $(log_value "DNS" "")
+dns=$(cat $MOUNT_POINT/etc/resolv.conf | egrep -v "^#")
+echo "$dns"
+
+echo
+
+# -----------------------------------------------------------
+# LAST MODIFIED
+# -----------------------------------------------------------
+echo $(log_header "LAST MODIFIED")
+echo
+
+last_modified="$(find $MOUNT_POINT -type f -printf '%TY-%Tm-%Td %TH:%TM:%TS %p\n' 2>/dev/null | sort | tail -n 20)"
+echo "$last_modified"
+
+echo
+
+# -----------------------------------------------------------
+# SESSIONS
+# -----------------------------------------------------------
+echo $(log_header "SESSIONS")
+echo
+
+if [ -e $MOUNT_POINT/var/log/auth.log ]; then
+	grep "session opened" $MOUNT_POINT/var/log/auth.log | awk '{print $1, $2, $3, $9, $10, $11}'
+	grep "session closed" $MOUNT_POINT/var/log/auth.log | awk '{print $1, $2, $3, $9, $10, $11}'
+elif [ -e $MOUNT_POINT/var/log/secure ]; then
+	grep "session opened" $MOUNT_POINT/var/log/secure | awk '{print $1, $2, $3, $9, $10, $11}'
+	grep "session closed" $MOUNT_POINT/var/log/secure | awk '{print $1, $2, $3, $9, $10, $11}'
+else
+	echo "Cannot find session information..."
+fi
+
+echo
 
 echo -e "${RED}${DIV}| FINISHED |${DIV}${RESET}"
